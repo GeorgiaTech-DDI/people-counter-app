fd415472c9bd09f409315dc75aca6d6e784646ab
Amy Truong <avytrv@gmail.com>
2023-03-31 00:57:06 -0400

    Add Kotlin app that detects labels in an image

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "testapp52f4a03c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "identifyLabelsa134b644": {
+      "region": "string",
+      "type": "string"
+    }
+  }
+}
