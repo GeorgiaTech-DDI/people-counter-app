525ca2d1ede134b5334dcbf55f62c1c5e365739b
Amy Truong <avytrv@gmail.com>
2023-04-27 20:52:13 -0400

    Edit code to detect people only

--- app/src/main/java/vip/smart3makerspaces/peoplecounter/MainActivity.kt
+++ app/src/main/java/vip/smart3makerspaces/peoplecounter/MainActivity.kt
@@ -79,65 +79,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-/*    // function to get image URIs
-    private fun getImageURI(): ArrayList<Uri> {
-        val list = ArrayList<Uri>()
-        val projection = arrayOf(MediaStore.Images.Media.DATA)
-        val mCursor = contentResolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            null,
-            null,
-            null
-        )
-        // uses cursor object to iterate through image storage
-        mCursor.use { cursor ->
-            cursor?.let {
-                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-                while (cursor.moveToNext()) {
-                    val imagePath = cursor.getString(columnIndex)
-                    val imageFile = File(imagePath)
-                    if (imageFile.exists()) {
-                        val imageUri = Uri.fromFile(imageFile)
-                        list.add(imageUri)
-                    }
-                }
-            }
-        }
-        return list
-    }
-
-    private fun getBitmapFromUri() {
-        // list of image URIs
-        val list = getImageURI()
-        // loops through each uri and turns it into bitmap
-        list.forEach { item ->
-            val bitmap: Bitmap = if (Build.VERSION.SDK_INT < 28) {
-                MediaStore.Images.Media.getBitmap(contentResolver, item)
-            } else {
-                ImageDecoder.decodeBitmap(
-                    ImageDecoder.createSource(contentResolver, item)
-                )
-            }
-            // detect labels function
-            detectLabels(bitmap)
-            // deletes image after use
-            contentResolver.delete(item, null, null)
-        }
-    }
-    private fun timer() {
-        val timer = Timer()
-        val delay = 0L // Start immediately
-        val period = 5 * 60 * 1000L // 5 minutes in milliseconds
-
-        timer.scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                getBitmapFromUri()
-            }
-        }, delay, period)
-    }*/
-
-    private suspend fun compressImageFromUri(uri: Uri): Bitmap {
+    private suspend fun compressImage(uri: Uri): Bitmap {
         // Run code on I/O thread instead of main thread
         return withContext(Dispatchers.IO) {
             // Create a temp file from the given URI
@@ -185,36 +127,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun detectLabels(image: Bitmap) {
+    private fun detectPeople(image: Bitmap): Int {
+        var peopleDetected = -1
         Amplify.Predictions.identify(IdentifyActionType.DETECT_LABELS, image,
             { result ->
                 val identifyResult = result as IdentifyLabelsResult
 
                 for (label in identifyResult.labels) {
-                    // Log label properties
-                    Log.i(TAG, "Name: ${label.name}")
-                    Log.i(TAG, "Confidence: ${label.confidence}")
-                    Log.i(TAG, "Box: ${label.box}")
-                    Log.i(TAG, "Id: ${label.id}")
-                    Log.i(TAG, "Polygon: ${label.polygon}")
-                    Log.i(TAG, "Value: ${label.value}")
-                    Log.i(TAG, "Type alias: ${label.typeAlias}")
-                    Log.i(TAG, "Boxes:")
-
-                    for (box in label.boxes) {
-                        // Append label bounding boxes
-                        Log.i(TAG, "Box: $box")
-                    }
-
-                    // Log parent labels
-                    Log.i(TAG, "Parent labels:")
-                    for (parentLabel in label.parentLabels) {
-                        Log.i(TAG, "Parent label: $parentLabel")
+                    if (label.name == "Person") {
+                        Log.i(TAG, "Detected ${label.boxes.size} person(s)")
+                        peopleDetected = label.boxes.size
                     }
                 }
             },
             { Log.e(TAG, "Label detection failed", it) }
         )
+        return peopleDetected
     }
 
     private fun togglePhotoStream() {
@@ -281,8 +209,9 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, msg)
                     lifecycleScope.launch(Dispatchers.IO) {
                         output.savedUri?.let {
-                            val compressedBitmap = compressImageFromUri(it)
-                            detectLabels(compressedBitmap)
+                            val compressedBitmap = compressImage(it)
+                            detectPeople(compressedBitmap)
+                            contentResolver.delete(it, null, null)
                         }
                     }
                 }
