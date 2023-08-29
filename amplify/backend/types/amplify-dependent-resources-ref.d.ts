a880c18f406f85850d3399ea6aad0ef58e8f31fd
Amy Truong <avytrv@gmail.com>
2023-08-28 23:17:41 -0400

    Add Amplify config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "peoplecounterdc177df7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "identifyLabels5be61175": {
+      "region": "string",
+      "type": "string"
+    }
+  }
+}
